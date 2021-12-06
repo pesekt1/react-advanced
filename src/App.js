9e73f062aa4b834da83b3f7ae8f6fe0c64e56d32
Tomas Pesek <pesekt@gmail.com>
2021-12-06 10:37:53 +0100

    multiple contexts

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
 import Login from "./context/Login";
 import Logout from "./context/Logout";
+import CartContext from "./context/CartContext";
 
 class App extends React.Component {
   state = { currentUser: null };
@@ -24,23 +25,26 @@ class App extends React.Component {
   render() {
     return (
       <div className="App-body">
-        <Movie id={1} />
-        <div>-------------------------------------------</div>
-        <Counter />
-        <div>-------------------------------------------</div>
-        <Users />
-        <div>-------------------------------------------</div>
-        <UserContext.Provider
-          value={{
-            currentUser: this.state.currentUser,
-            onLogIn: this.handleLogIn,
-            onLogout: this.handleLogOut,
-          }}
-        >
-          <MoviePage />
-          {!this.state.currentUser && <Login />}
-          {this.state.currentUser && <Logout />}
-        </UserContext.Provider>
+        <CartContext.Provider value={{ cart: [] }}>
+          <Movie id={1} />
+          <div>-------------------------------------------</div>
+          <Counter />
+          <div>-------------------------------------------</div>
+          <Users />
+          <div>-------------------------------------------</div>
+          <UserContext.Provider
+            value={{
+              currentUser: this.state.currentUser,
+              onLogIn: this.handleLogIn,
+              onLogout: this.handleLogOut,
+            }}
+          >
+            <MoviePage />
+            {!this.state.currentUser && <Login />}
+            {this.state.currentUser && <Logout />}
+          </UserContext.Provider>
+          <div>-------------------------------------------</div>
+        </CartContext.Provider>
       </div>
     );
   }
--- src/context/MovieRow.jsx
+++ src/context/MovieRow.jsx
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
 import UserContext from "./userContext";
+import CartContext from "./CartContext";
 
 function MovieRow(props) {
-  const context = useContext(UserContext);
-  console.log("context", context.currentUser);
+  const userContext = useContext(UserContext);
+  console.log("userContext", userContext);
+
+  const cartContext = useContext(CartContext);
+  console.log("cartContext", cartContext);
 
   return (
     <div>
-      Movie Row for user: {context.currentUser ? context.currentUser.name : ""}
+      Movie Row for user:{" "}
+      {userContext.currentUser ? userContext.currentUser.name : ""}
     </div>
   );
 }
